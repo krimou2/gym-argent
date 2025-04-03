ce83c1b7e24d65dbf93f1fdd36f4d8af5220275a
Abdelkrim Achouri <111244828+krimou2@users.noreply.github.com>
2025-04-02 21:43:37 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://krimou21:Karim1998@krimou21.mysql.pythonanywhere-services.com$gym'
+        'mysql+pymysql://root:your_password@localhost/gym_argente'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOTSTRAP_SERVE_LOCAL = True
